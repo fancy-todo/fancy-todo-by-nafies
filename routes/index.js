f01ec82e83c05904d4f695edfe4d20ab7afcc2c9
nafies1 <ghuroba.muslim@gmail.com>
2020-02-10 18:03:29 +0700

    fix bug in dotenv

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const todos = require('./todos')
 const auth = require('./auth')
 
+router.get('/', (req, res) => {
+  res.status(200).json('Welcome to fancy-todo API')
+})
 router.use('/todos', todos)
 router.use('/auth', auth)
 
